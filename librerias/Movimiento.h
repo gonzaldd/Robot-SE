88a489a636f426bcf10f57437076dace577d13a1
truccomarcos <truccomarcos@gmail.com>
2016-10-05 15:55:06 -0300

    Agregada libreria estrategias

--- librerias/Movimiento.h
+++ librerias/Movimiento.h
@@ -12,43 +12,64 @@
 
 #include "DcMotor.h"
 
+
+//frenar: Detener los motores
 void frenar();
 
+//avanzar_full: Encender los motores a la velocidad_full
 void avanzar_full();
 
+//avanzar_media: Encender los motores a la velocidad_media
 void avanzar_media();
 
+//avanzar_baja: Encender los motores a la velocidad_baja
 void avanzar_baja();
 
+//reversa_full: Encender los motores a la -velocidad_full
 void reversa_full();
 
+//reversa_media: Encender los motores a la -velocidad_media
 void reversa_media();
 
+//reversa_baja: Encender los motores a la -velocidad_baja
 void reversa_baja();
 
+//avanzar_full_der: Encender el motor izquierdo a velocidad_full y el derecho a velocidad_media
 void avanzar_full_der();
 
+//avanzar_full_izq: Encender el motor derecho a velocidad_full y el derecho a velocidad_media
 void avanzar_full_izq();
 
+//avanzar_media_der: Encender el motor izquierdo a velocidad_media y el derecho a velocidad_baja
 void avanzar_media_der();
 
+//avanzar_media_izq: Encender el motor derecho a velocidad_media y el derecho a velocidad_baja
 void avanzar_media_izq();
 
+//reversa_full_der: Encender el motor izquierdo a -velocidad_full y el derecho a -velocidad_media
 void reversa_full_der();
 
+//reversa_full_izq: Encender el motor derecho a -velocidad_full y el izquierdo a -velocidad_media
 void reversa_full_izq();
 
+//reversa_media_der: Encneder el motor derecho a -velocidad_media y el izquierdo -velocidad_baja
 void reversa_media_der();
 
+//reversa_media_izq: Encneder el motor izquierdo a -velocidad_media y el derecho -velocidad_baja
 void reversa_media_izq();
 
+//Hacer un giro_der de 360 grados
 void girar_360_der();
 
-//Hay que ajustar los delays, girar a la izquierda y a la dercha necesitan distinto delay
+//TODO Hay que ajustar los delays, girar a la izquierda y a la dercha necesitan distinto delay
+//Hacer un giro_izq de 360 grados
 void girar_360_izq();
 
+//Hacer un giro_der de 180 grados
 void girar_180_der();
-//Hay que ajustar los delays, girar a la izquierda y a la dercha necesitan distinto delay
+
+//TODO Hay que ajustar los delays, girar a la izquierda y a la dercha necesitan distinto delay
+//Hacer un giro_izq de 180 grados
 void girar_180_izq();
 
 #endif
--- librerias/Sensores.h
+++ librerias/Sensores.h
@@ -4,6 +4,21 @@
 #define distancia_max 25
 #define distancia_min 2
 
+
+//Sensores en pista de cartulina:
+
+#define sensor_izq_blanco 850
+#define sensor_izq_negro 400
+
+#define sensor_der_blanco 600
+#define sensor_der_negro 200
+
+#define sensor_tras_blanco 300
+#define sensor_tras_negro 100
+
+
+//Sensores en pista de madera:
+/*
 #define sensor_izq_blanco 700   //sensor_izq Blanco si mayor a
                                 //sensor_izq Gris entre 100 y 800
 #define sensor_izq_negro 100    //sensor_izq Negro si menor a
@@ -15,9 +30,12 @@
 #define sensor_tras_blanco 200  //sensor_tras Blanco si mayor a
                                 //sensor_tras Gris entre 40 y 180
 #define sensor_tras_negro 30    //sensor_tras Negro si menor a
+*/
 
-#define sensor_US_distancia_max 25  //Distancia
+#define sensor_US_distancia_max 25  //Si el obstáculo está en ese rango
 #define sensor_US_distancia_min 10
+
+/* Devuelve el valor que mide el sensor*/
 
 float sensor_US();
 
@@ -29,10 +47,14 @@ float sensor_tras();
 
 int sensor_IR();
 
+/*test_blanco revisa si los sensores de adelante estan en blanco*/
 boolean test_blanco();
 
+/*test_blanco_izq revisa si el sensor izquierdo está sobre blanco*/
 boolean test_blanco_izq();
 
+/*test_blanco_der revisa si el sensor derecho está sobre blanco*/
 boolean test_blanco_der();
 
+/*test_blanco_tras revisa si el sensor trasero está sobre blanco*/
 boolean test_blanco_tras();
