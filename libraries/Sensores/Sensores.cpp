da575d652614a4d25aae1ee72d763bbd1df82d39
sebastian-bascu <sebastian.bascu@gmail.com>
2016-10-11 13:33:35 -0300

    Sen.cpp al dia 11oct2016

--- libraries/Sensores/Sensores.cpp
+++ libraries/Sensores/Sensores.cpp
@@ -11,7 +11,7 @@ float Sensores::sensor_US()
 {
   return ping.measureCM();
 }
-
+/*
 float Sensores::Sensores::sensor_izq()
 {
   return analogRead(2);
@@ -26,44 +26,50 @@ float Sensores::sensor_tras()
 {
   return analogRead(5);
 }
+*/
 
+float Sensores::Sensores::sensor_izq()
+{
+  return map(analogRead(A2), 0, 1023, 0.0, 5.0);
+}
+
+float Sensores::sensor_der()
+{
+  return map(analogRead(A3), 0, 1023, 0.0, 5.0);
+}
+
+float Sensores::sensor_tras()
+{
+  return map(analogRead(A5), 0, 1023, 0.0, 5.0);
+}
 
 boolean Sensores::test_blanco()
-{
+{ /*
   boolean retorno = false;
-  if(sensor_izq() > sensor_izq_blanco || sensor_der() > sensor_der_blanco)
+  if(sensor_izq() >= sensor_izq_blanco || sensor_der() >= sensor_der_blanco)
   {
     delay(100);
-    if(sensor_izq() > sensor_izq_blanco || sensor_der() > sensor_der_blanco)
+    if(sensor_izq() >= sensor_izq_blanco || sensor_der() >= sensor_der_blanco)
     {
       retorno = true;
     }
-  }
-  return retorno;
+  }*/
+  return (sensor_der() >= 2 || sensor_izq() > 3);
 }
 
 boolean Sensores::test_blanco_izq()
 {
-  return (sensor_izq() > sensor_izq_blanco && sensor_der() < sensor_der_negro);
+  return (sensor_izq() >= sensor_izq_blanco && sensor_der() <= sensor_der_negro);
 }
 
 boolean Sensores::test_blanco_der()
 {
-  return (sensor_izq() < sensor_izq_negro && sensor_der() > sensor_der_blanco);
+  return (sensor_izq() <= sensor_izq_negro && sensor_der() >= sensor_der_blanco);
 }
 
 boolean Sensores::test_blanco_tras()
 {
-  boolean retorno = false;
-  if(sensor_tras() > sensor_tras_blanco)
-  {
-    delay(50);
-    if(sensor_tras() > sensor_tras_blanco)
-    {
-      retorno = true;
-    }
-  }
-  return retorno;
+  return !(sensor_tras() < 1);
 }
 
 void Sensores::prueba_sensores()
@@ -74,6 +80,12 @@ void Sensores::prueba_sensores()
   Serial.println(sensor_der());
   Serial.println("Sensor US");
   Serial.println(sensor_US());
+  Serial.println("Sensor Trasero");
+  Serial.println(sensor_tras());
+  if(test_blanco())
+    Serial.println("Está en blanco");
+  else
+    Serial.println("No está en blanco");
   delay(1500);
 
 }
