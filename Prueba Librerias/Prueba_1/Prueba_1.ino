c671dcbf02ed4d18d9e9b03b43bedc4dcb5a1ccf
sebastian-bascu <sebastian.bascu@gmail.com>
2016-10-11 13:19:38 -0300

    Prueba_1.ino al día 11oct2016

--- Prueba Librerias/Prueba_1/Prueba_1.ino
+++ Prueba Librerias/Prueba_1/Prueba_1.ino
@@ -4,7 +4,7 @@
 #include <Movimiento.h>
 #include <Sensores.h>
 #include <Estrategias.h>
-
+  
 
 DCMotor motor0(M0_EN, M0_D0, M0_D1, false);
 DCMotor motor1(M1_EN, M1_D0, M1_D1, false);
@@ -17,7 +17,7 @@ PingSensor ping(A1);
 Movimiento mov = Movimiento(motor0, motor1);
 Sensores sen = Sensores(irReceiver,ping);
 Estrategias est = Estrategias(sen,mov);
-
+int codigoActivo;
 
 
 void setup()
@@ -27,34 +27,32 @@ void setup()
   digitalWrite(25, HIGH);
   digitalWrite(11, LOW);
   digitalWrite(10, LOW);
-  delay(5000);
-  
+  delay(1000); 
 
 }
 
 void loop()
 {
-//4
-est.escapar_y_frenar();
-//    sen.prueba_sensores();
-    /*
-    while(!sen.test_blanco())
-    {
-      mov.avanzar_baja();
+  int code = irReceiver.getIRRemoteCode();
+  
+  if(code == 1 || code == 2 || code == 3 || code == 4 || code == 5 || code == 6 || code == 7 || code == 8 || code == 9){
+    codigoActivo = code;
+    digitalWrite(13, LOW);
+    if(code == 2){
+      digitalWrite(13, HIGH);
     }
-    Serial.println("Test blanco izq");
-    Serial.println(sen.test_blanco_izq());
-    Serial.println("Test blanco der");
-    Serial.println(sen.test_blanco_der());
+  }
+  
+  if( codigoActivo == 1){
+    est.recorrer();
+  }else if(codigoActivo == 2){
+    est.recorrerAtacar();
+  }else if(codigoActivo == 3 ){
     mov.frenar();
-    delay(3000);*/
-/*
-Serial.println("Sensor Izq:");
-Serial.println(sen.sensor_izq());
-Serial.println("Sensor Der:");
-Serial.println(sen.sensor_der());
-Serial.println("Sensor Tras:");
-Serial.println(sen.sensor_tras());
-delay(1500);
-*/
+  }else if(codigoActivo == 4){
+    est.recorrer_random();
+  }else if(codigoActivo == 5){
+    est.recorrerEscapar();
+  }
+  
 }
