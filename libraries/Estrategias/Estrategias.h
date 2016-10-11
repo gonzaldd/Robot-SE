a7a0d9f0743fde0191de4277684077e7ae39b806
sebastian-bascu <sebastian.bascu@gmail.com>
2016-10-11 13:28:18 -0300

    Estr.h al dia 11oct2016

--- libraries/Estrategias/Estrategias.h
+++ libraries/Estrategias/Estrategias.h
@@ -3,10 +3,30 @@
 
 #include "Movimiento.h"
 #include "Sensores.h"
-#define sensor_US_distancia_max 35  //Si el obstáculo está en ese rango
+#define sensor_US_distancia_max 15   //Si el obstáculo está en ese rango
 #define sensor_US_distancia_min 10
 #define sensor_US_saturado 3000 //Tira valores mayores a 3000 cuando esta pegado
 #define sensor_US_nada 150
+
+#define c 261
+#define d 294
+#define e 329
+#define f 349
+#define g 391
+#define gS 415
+#define a 440
+#define aS 455
+#define b 466
+#define cH 523
+#define cSH 554
+#define dH 587
+#define dSH 622
+#define eH 659
+#define fH 698
+#define fSH 740
+#define gH 784
+#define gSH 830
+#define aH 880
 
 
 
@@ -56,6 +76,9 @@ class Estrategias
       void giro_izq_y_atacar();
 
       void recorrer_y_atacar(); //Recorrer la pista, si detecta objetivo, atacar.
+      void recorrer_random();
+      void recorrerAtacar();
+      void recorrerEscapar();
 
       //Estrategias Mariconas
       void escapar_y_frenar();
@@ -63,6 +86,8 @@ class Estrategias
 
       //Estrategias Pasivo Agresivas
       void escapar_y_atacar();
+      void cancion_starwars();
+      void cancion_mario();
 
 
       boolean condicion_frenado_avanzar();
