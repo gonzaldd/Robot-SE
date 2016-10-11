0d902c3627fa6f8e7c727f49726c58ce1ad17e00
sebastian-bascu <sebastian.bascu@gmail.com>
2016-10-11 13:34:59 -0300

    sen.h al dia 11oct2016

--- libraries/Sensores/Sensores.h
+++ libraries/Sensores/Sensores.h
@@ -9,24 +9,24 @@
 
 
 //Sensores en pista de cartulina:
+/*
+#define sensor_izq_blanco 700
+#define sensor_izq_negro 600
 
-#define sensor_izq_blanco 850
-#define sensor_izq_negro 400
+#define sensor_der_blanco 600
+#define sensor_der_negro 500
 
-#define sensor_der_blanco 480
-#define sensor_der_negro 300
-
-#define sensor_tras_blanco 200
-#define sensor_tras_negro 180
-
+#define sensor_tras_blanco 500
+#define sensor_tras_negro 400
+*/
 
 //Sensores en pista de madera:
 /*
-#define sensor_izq_blanco 700   //sensor_izq Blanco si mayor a
+#define sensor_izq_blanco 300  //sensor_izq Blanco si mayor a
                                 //sensor_izq Gris entre 100 y 800
 #define sensor_izq_negro 100    //sensor_izq Negro si menor a
 
-#define sensor_der_blanco 500   //sensor_der Blanco si mayor a
+#define sensor_der_blanco 390   //sensor_der Blanco si mayor a
                                 //sensor_der Gris entre 90 y 900
 #define sensor_der_negro 90     //sensor_der Negro si menor a
 
@@ -34,6 +34,18 @@
                                 //sensor_tras Gris entre 40 y 180
 #define sensor_tras_negro 30    //sensor_tras Negro si menor a
 */
+#define sensor_izq_blanco 3  //sensor_izq Blanco si mayor a
+                                //sensor_izq Gris entre 100 y 800
+#define sensor_izq_negro 0    //sensor_izq Negro si menor a
+
+#define sensor_der_blanco 3   //sensor_der Blanco si mayor a
+                                //sensor_der Gris entre 90 y 900
+#define sensor_der_negro 0     //sensor_der Negro si menor a
+
+#define sensor_tras_blanco 2  //sensor_tras Blanco si mayor a
+                                //sensor_tras Gris entre 40 y 180
+#define sensor_tras_negro 0
+
 
 
 
